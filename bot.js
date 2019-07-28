59053ff13d31343f0bd6cfa20b50abdf056d37a4
MonsTis <45233355+hggfa@users.noreply.github.com>
2019-07-28 19:12:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`MonsTis/ Best Gamer`,"http://twitch.tv/Dream")
+client.user.setGame(`MonsTis/ Occupé`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
